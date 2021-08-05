0c87ae4e9d9f522a2ed149de810265802a3f3c60
Dami Kim <dami.k.dev@gmail.com>
2021-08-06 00:18:49 +0900

    chore: 불필요한 폴더 삭제

--- 210719-design-pattern /public/index.js
+++
@@ -1,3 +0,0 @@
-import App from "../src/App.js";
-// const $app = document.querySelector("#app"); //$app넣으면 querySelector 안먹힘
-new App(document.querySelector("#app"));
--- 210719-design-pattern /src/App.js
+++
@@ -1,7 +0,0 @@
-import MainPage from "./pages/MainPage.js";
-
-export default class App {
-	constructor($target) {
-		new MainPage($target);
-	}
-}
